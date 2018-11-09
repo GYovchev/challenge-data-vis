000b36b90fd399fd81103354bcfe3064c0ac915e
Georgi Yovchev <georgi.yovchev@sciant.com>
2018-11-09 05:08:34 +0200

    Create working version

---
+++ infobox.jsx
@@ -0,0 +1,26 @@
+var React = require('react')
+var createReactClass = require('create-react-class')
+
+module.exports = createReactClass({
+  render () {
+    var tableStyle={
+        borderCollapse: "separate",
+        borderSpacing: "2px",
+        borderColor: "grey" 
+    };
+    return (<div>
+        {Object.keys(this.props.data).length > 0 && (
+        <table style={tableStyle}>
+            <tbody>
+                <tr><td>i:</td><td>{this.props.data.i}</td></tr>
+                <tr><td>species:</td><td>{this.props.data.species}</td></tr>
+                <tr><td>petalWidth:</td><td>{this.props.data.petalWidth}</td></tr>
+                <tr><td>petalLength</td><td>{this.props.data.petalLength}</td></tr>
+                <tr><td>sepalWidth</td><td>{this.props.data.sepalWidth}</td></tr>
+                <tr><td>sepalLength</td><td>{this.props.data.sepalLength}</td></tr>
+            </tbody>
+        </table>
+        )}
+</div>)
+  }
+})
--- scatter.jsx
+++ scatter.jsx
@@ -1,10 +1,43 @@
-var React = require('React')
+var React = require('react')
 var linmap = require('linmap')
 var jsonist = require('jsonist')
 var createReactClass = require('create-react-class')
 
+var Plot = require('./plot.jsx');
+var InfoBox = require('./infobox.jsx');
+
 module.exports = createReactClass({
+  getInitialState() {
+    return {
+      plotData: [],
+      infoBoxData: {}
+     };
+  },
+  componentDidMount () {
+    jsonist.get(window.location+"public/iris.json", {}, (err, data, resp) => {
+      if(err){
+        alert("Couldnt load data:\n"+err);
+      }
+      this.setState({plotData: data, infoBoxData: {}});
+    })
+  },
+  onMouseEnterPoint(pointData){
+    this.setState({infoBoxData: pointData})
+  },
   render () {
-    return <div />
+    var canvasStyle={
+      height: this.props.height, 
+      width: this.props.width,
+      backgroundColor: "#222",
+      boxShadow: "0px 3px 8px rgba(0, 0, 0, 0.5)",
+      border: "1px solid black",
+      position: "relative",
+      color: "rgba(255, 255, 255, 0.7)" 
+    };
+    return (<div style={canvasStyle}>
+      <Plot data={this.state.plotData} height={this.props.height} 
+            width={this.props.width} onMouseEnterPoint={this.onMouseEnterPoint}/>
+      <InfoBox data={this.state.infoBoxData}/>
+      </div>)
   }
 })
